4a0e0e1082e358cee9368f2323c92afe94a0fcaa
Craig Chaffee <cchaffee@mitre.org>
2018-04-26 09:47:03 -0400

    Some 7 controls
    Signed-off-by: Craig Chaffee <cchaffee@mitre.org>

--- controls/M-7.5.rb
+++ controls/M-7.5.rb
@@ -1,3 +1,37 @@
+TOMCAT_SERVICE_NAME= attribute(
+  'tomcat_service_name',
+  description: 'Name of Tomcat service',
+  default: 'tomcat'
+)
+
+TOMCAT_CONF_SERVER= attribute(
+  'tomcat_conf_server',
+  description: 'Path to tomcat server.xml',
+  default: '/usr/share/tomcat/conf/server.xml'
+)
+
+TOMCAT_APP_DIR= attribute(
+  'tomcat_app_dir',
+  description: 'location of tomcat app directory',
+  default: '/var/lib/tomcat'
+)
+
+TOMCAT_CONF_WEB= attribute(
+  'tomcat_conf_web',
+  description: 'location of tomcat web.xml',
+  default: '/usr/share/tomcat/conf/web.xml'
+)
+
+TOMCAT_HOME= attribute(
+  'tomcat_home',
+  description: 'location of tomcat home directory',
+  default: '/usr/share/tomcat'
+)
+
+only_if do
+  service(TOMCAT_SERVICE_NAME).installed?
+end
+
 control "M-7.5" do
   title "7.5 Ensure pattern in context.xml is correct (Scored)"
   desc  "The pattern setting informs Tomcat what information should be logged
@@ -31,4 +65,17 @@ pattern='%h %t %H cookie:%{SESSIONID}c request:%{SESSIONID}r %m %U %s %q %r'
 />
 "
   tag "Default Value": "Does not exist by default\n"
+
+  begin
+    context_xml = command("ls #{TOMCAT_HOME}/webapps/*/META-INF/context.xml").stdout.split.each do |web_file|
+      describe xml(web_file) do
+        its('Context/Valve/attribute::className') { should include "org.apache.catalina.valves.AccessLogValve" }
+        its('Context/Valve/attribute::directory') { should cmp '$CATALINA_HOME/logs/' }
+        its('Context/Valve/attribute::prefix') { should cmp 'access_log' }
+        its('Context/Valve/attribute::fileDateFormat') { should cmp 'yyyy-MM-dd.HH' }
+        its('Context/Valve/attribute::suffix') { should cmp '.log' }
+        its('Context/Valve/attribute::pattern') { should cmp '%h %t %H cookie:%{SESSIONID}c request:%{SESSIONID}r %m %U %s %q %r' }
+      end
+    end
+  end
 end
